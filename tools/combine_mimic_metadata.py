5df436076598dd9dabdc734e46a328f18ee42c63
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-08-25 23:19:16 +0300

    [Fix]: fix output dataframe and add logger

--- tools/combine_mimic_metadata.py
+++ tools/combine_mimic_metadata.py
@@ -1,59 +1,106 @@
-import pandas as pd
 import os
+import logging
 import argparse
+from pathlib import Path
 
+import pandas as pd
 from utils import get_file_list
+
+os.makedirs('logs', exist_ok=True)
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d.%m.%Y %H:%M:%S',
+    filename='logs/{:s}.log'.format(Path(__file__).stem),
+    filemode='w',
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
 
 
 def combine_metadata(
         metadata_csv: str,
-        findings_path: str,
+        findings_csv: str,
         split_csv: str,
-        output_dir: str,
+        save_dir: str,
         dataset_dir: str = None,
 ) -> None:
-    output_dir = os.path.join(output_dir, 'combined_metadata')
-    os.makedirs(output_dir, exist_ok=True)
 
-    findings = pd.read_csv(findings_path, compression='gzip')
-    findings_name= os.path.basename(findings_path)
-    file_name=os.path.splitext(findings_name)[0]
-    metadata = pd.read_csv(metadata_csv, compression='gzip')
-    split = pd.read_csv(split_csv, compression='gzip')
-    meta_findings = metadata.merge(findings, on=["subject_id", "study_id"], how="left")
-    extracted_col = split["split"]
-    combined_metadata = meta_findings.join(extracted_col)
-    #s3 = boto3.resource('s3')
-    #my_bucket = s3.Bucket('s3://qtr-ml-projects/edema_quantification/dataset/MIMIC-CXR/files/')
+    df_findings = pd.read_csv(findings_csv, compression='gzip')
+    df_metadata = pd.read_csv(metadata_csv, compression='gzip')
+    df_split = pd.read_csv(split_csv, compression='gzip')
+    os.makedirs(save_dir, exist_ok=True)
+
+    _df_out = df_metadata.merge(df_findings, on=['subject_id', 'study_id'], how='left')
+    df_out = _df_out.join(df_split['split'])
+
+    logger.info(f'Metadata CSV..............: {metadata_csv}')
+    logger.info(f'Findings CSV..............: {findings_csv}')
+    logger.info(f'Split CSV.................: {split_csv}')
+    logger.info(f'Dataset dir...............: {dataset_dir}')
+    logger.info(f'Save dir..................: {save_dir}')
+
     if dataset_dir:
         img_paths = get_file_list(
             src_dirs=dataset_dir,
-            ext_list=['.png', '.jpg', '.jpeg', '.bmp'])
-        dicom_id = list()
-        for i in img_paths:
-            img_name = os.path.basename(i)
-            dicom_id.append(os.path.splitext(img_name)[0])
+            ext_list=[
+                '.png',
+                '.jpg',
+                '.jpeg',
+                '.bmp',
+            ]
+        )
+        num_images = len(img_paths)
 
-        files = pd.DataFrame(list(zip(dicom_id, img_paths)), columns=['dicom_id', 'file_path'])
-        combined_metadata = combined_metadata.merge(files, on=["dicom_id"], how="left")
+        dicom_ids = [str(Path(img_path).stem) for img_path in img_paths]
+        df_paths = pd.DataFrame(list(zip(dicom_ids, img_paths)), columns=['dicom_id', 'Path'])
+        df_out = df_out.merge(df_paths, on=['dicom_id'], how='left')
+    else:
+        num_images = None
 
-    #combined_metadata.to_csv(os.path.join(output_dir, f'combined_metadata_{file_name}.csv'), index=1, encoding='utf-8')
-    combined_metadata.to_excel(os.path.join(output_dir, f'combined_metadata_{file_name}.xlsx'), index=False)
+    logger.info(f'Number of images..........: {num_images}')
+
+    suffix = 'chexpert' if 'chexpert' in findings_csv else 'negbio'
+    meta_path = os.path.join(save_dir, f'metadata_{suffix}.csv')
+    cols = {
+        'dicom_id': 'DICOM ID',
+        'subject_id': 'Subject ID',
+        'study_id': 'Study ID',
+        'PerformedProcedureStepDescription': 'Description',
+        'ViewPosition': 'View',
+        'Rows': 'Height',
+        'Columns': 'Width',
+        'StudyDate': 'Study Date',
+        'StudyTime': 'Study Time',
+        'ProcedureCodeSequence_CodeMeaning': 'Procedure Code',
+        'ViewCodeSequence_CodeMeaning': 'View Code',
+        'PatientOrientationCodeSequence_CodeMeaning': 'Patient Orientation',
+        'split': 'Split',
+    }
+    df_out.rename(columns=cols, inplace=True)
+    df_out.index += 1
+    df_out.to_csv(
+        meta_path,
+        index=True,
+        index_label='ID',
+        encoding='utf-8',
+    )
+    logger.info('Complete')
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Combine_metadata')
-    parser.add_argument('--metadata_csv', default='dataset/metadata/mimic-cxr-2.0.0-metadata.csv', type=str)
-    parser.add_argument('--findings_path', default='dataset/metadata/mimic-cxr-2.0.0-chexpert.csv', type=str)
-    parser.add_argument('--split_csv', default='dataset/metadata/mimic-cxr-2.0.0-split.csv', type=str)
-    parser.add_argument('--output_dir', default='dataset/output', type=str)
-    parser.add_argument('--dataset_dir', type=str, default=None)
+    parser = argparse.ArgumentParser(description='Combine metadata')
+    parser.add_argument('--metadata_csv', default='dataset/mimic/mimic-cxr-2.0.0-metadata.csv.gz', type=str)
+    parser.add_argument('--findings_path', default='dataset/mimic/mimic-cxr-2.0.0-chexpert.csv.gz', type=str)
+    parser.add_argument('--split_csv', default='dataset/mimic/mimic-cxr-2.0.0-split.csv.gz', type=str)
+    parser.add_argument('--dataset_dir', default=None, type=str)
+    parser.add_argument('--save_dir', default='dataset/mimic', type=str)
     args = parser.parse_args()
+
     combine_metadata(
         metadata_csv=args.metadata_csv,
-        findings_path=args.findings_path,
+        findings_csv=args.findings_path,
         split_csv=args.split_csv,
-        output_dir=args.output_dir,
+        save_dir=args.save_dir,
         dataset_dir=args.dataset_dir,
     )
