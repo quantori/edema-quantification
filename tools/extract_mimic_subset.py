a64f401868009c2a78d63ad12aee588261329054
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-09-02 22:56:33 +0300

    [Feature]: implement extraction of the MIMIC subset

--- tools/extract_mimic_subset.py
+++ tools/extract_mimic_subset.py
@@ -27,47 +27,56 @@ def process_single_study(
 ) -> pd.DataFrame:
     _, df_study = group
     df_study.reset_index(drop=True, inplace=True)
+    subject_id = df_study.iloc[0]['Subject ID']
+    study_id = df_study.iloc[0]['Study ID']
 
-    # FIXME: revise file copying
     # Process study with paired images
-    if len(df_study) == 2 and save_pairs_only:
+    study_views = [df_study.iloc[idx]['View'] for idx in range(len(df_study))]
+    is_correct_view = (
+            ('AP' in study_views) and ('LATERAL' in study_views)
+            or ('PA' in study_views) and ('LATERAL' in study_views)
+            or ('AP' in study_views) and ('LL' in study_views)
+            or ('PA' in study_views) and ('LL' in study_views)
+    )
 
-        study_views = [df_study.iloc[idx]['View'] for idx in range(len(df_study))]
-        if (
-                ('AP' in study_views) and ('LATERAL' in study_views)
-                or ('PA' in study_views) and ('LATERAL' in study_views)
-                or ('AP' in study_views) and ('LL' in study_views)
-                or ('PA' in study_views) and ('LL' in study_views)
-        ):
-            subject_id = df_study.iloc[0]['Subject ID']
-            study_id = df_study.iloc[0]['Study ID']
-            study_dir = os.path.join(save_dir, f'{subject_id}', f'{study_id}')
+    # Save paired images of a study
+    study_dir = os.path.join(save_dir, f'{subject_id}', f'{study_id}')
+    if save_pairs_only:
+
+        if is_correct_view and len(df_study) == 2:
             os.makedirs(study_dir, exist_ok=True)
-            for idx, src_path in df_study['Path'].iteritems():
+            for idx, src_path in df_study['Image path'].iteritems():
                 img_name = Path(src_path).name
                 dst_path = os.path.join(study_dir, img_name)
-                df_study.at[idx, 'Path'] = dst_path
+                df_study.at[idx, 'Image path'] = dst_path
                 df_study.at[idx, 'Image name'] = img_name
                 try:
                     shutil.copy(src_path, dst_path)
-                except shutil.SameFileError:
-                    logger.info('Source and destination represents the same file')
-                except PermissionError:
-                    logger.info('Permission denied')
                 except Exception as e:
-                    logger.info(f'Error occurred while copying file: {img_name}')
+                    logger.info(f'Error occurred while copying {img_name}')
+            return df_study
 
-    # FIXME: change this case
+        else:
+            return pd.DataFrame([])
+
+    # Save all images of a study
     else:
-        df_study = pd.DataFrame([2])
-
-    return df_study
+        os.makedirs(study_dir, exist_ok=True)
+        for idx, src_path in df_study['Image path'].iteritems():
+            img_name = Path(src_path).name
+            dst_path = os.path.join(study_dir, img_name)
+            df_study.at[idx, 'Image path'] = dst_path
+            df_study.at[idx, 'Image name'] = img_name
+            try:
+                shutil.copy(src_path, dst_path)
+            except Exception as e:
+                logger.info(f'Error occurred while copying {img_name}')
+        return df_study
 
 
 def extract_subset(
         metadata_csv: str,
         disease: str,
-        view: str,
         save_pairs_only: bool,
         save_dir: str,
 ) -> None:
@@ -77,7 +86,6 @@ def extract_subset(
     Args:
         metadata_csv: a path to a MIMIC metadata CSV file
         disease: one of the diseases to be extracted
-        view: one of the image views i.e. PA (postero-anterior), LATERAL (lateral), or AP (antero-posterior)    # FIXME: remove
         save_pairs_only: if True, save only the paired cases (frontal and lateral images)
         save_dir: directory where the output files will be saved
 
@@ -89,25 +97,15 @@ def extract_subset(
 
     logger.info(f'Metadata CSV..............: {metadata_csv}')
     logger.info(f'Disease...................: {disease}')
-    # logger.info(f'View......................: {view}')
     logger.info(f'Save pairs only...........: {save_pairs_only}')
     logger.info(f'Save dir..................: {save_dir}')
 
-    # Read the source data frame and extract the required subset
-    # FIXME: remove
-    # df = pd.read_csv(metadata_csv)
-    # if view in VIEWS:
-    #     df_out = df[df[disease] == 1 & df['View'] == view]
-    # else:
-    #     df_out = df[df[disease] == 1]
+    # Read the source data frame and filter it by the required disease
     df = pd.read_csv(metadata_csv)
-    df_out = df[df[disease] == 1]
-    logger.info(f'Number of images..........: {len(df_out["DICOM ID"].unique())}')
-    logger.info(f'Number of subjects........: {len(df_out["Subject ID"].unique())}')
-    logger.info(f'Number of studies.........: {len(df_out["Study ID"].unique())}')
+    df_disease = df[df[disease] == 1]
 
-    # TODO: Processing of the required dataframe
-    groups = df_out.groupby(['Study ID'])
+    # Processing of the required dataframe
+    groups = df_disease.groupby(['Study ID'])
     processing_func = partial(
         process_single_study,
         save_dir=save_dir,
@@ -117,7 +115,22 @@ def extract_subset(
     result = Parallel(n_jobs=num_cores)(
         delayed(processing_func)(group) for group in tqdm(groups, desc='Dataset conversion', unit=' study')
     )
-    print(result)
+    df_out = pd.concat(result)
+    df_out.reset_index(drop=True, inplace=True)
+    df_out.sort_values(['Subject ID', 'Study ID', 'View'], inplace=True)
+    suffix = '_paired' if save_pairs_only else ''
+    save_path = os.path.join('dataset', f'metadata_{disease.lower()}{suffix}.csv')
+    df_out.to_csv(
+        save_path,
+        index=False,
+    )
+    logger.info(f'Source images.............: {len(df_disease["DICOM ID"].unique())}')
+    logger.info(f'Output images.............: {len(df_out["DICOM ID"].unique())}')
+    logger.info(f'Source subjects...........: {len(df_disease["Subject ID"].unique())}')
+    logger.info(f'Output subjects...........: {len(df_out["Subject ID"].unique())}')
+    logger.info(f'Source studies............: {len(df_disease["Study ID"].unique())}')
+    logger.info(f'Output studies............: {len(df_out["Study ID"].unique())}')
+    logger.info('Complete')
 
 
 if __name__ == '__main__':
@@ -138,13 +151,6 @@ if __name__ == '__main__':
         'Pneumothorax',
         'Support Devices',
     ]
-    # TODO: remove
-    # VIEWS = [
-    #     'PA',
-    #     'AP',
-    #     'LATERAL',
-    #     'LL',
-    # ]
 
     parser = argparse.ArgumentParser(description='Extract MIMIC subset')
     parser.add_argument('--metadata_csv', default='dataset/mimic/metadata_chexpert.csv', type=str)
