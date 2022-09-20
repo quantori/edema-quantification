8b53ccf845d4ad8f814dd6d59aeaec0d46a8f534
ant-str <makoveev90@gmail.com>
2022-09-20 22:39:18 +0200

    preprocess function and some comments are added

--- models_edema.py
+++ models_edema.py
@@ -6,40 +6,83 @@ The description to be filled...
 import torch
 from torch import nn
 import pytorch_lightning as pl
+from torchvision import transforms
 
 
 class SqueezeNet(nn.Module):
-    """SqueezeNet backbone.
+    """SqueezeNet encoder.
 
-    Includes the SqueezeNet architecture plus transient layers. The transient layers are required to
-    concatenate the main backbone layers with the prototype layer.
+    The pre-trained model expects input images normalized in the same way, i.e. mini-batches of
+    3-channel RGB images of shape (3 x H x W), where H and W are expected to be at least 224. The
+    images have to be loaded in to a range of [0, 1] and then normalized using
+    mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225].
 
     Args:
         nn (_type_): _description_
     """
 
-    def __init__(self):
+    def __init__(self, preprocessed: bool = True, pretrained: bool = True):
         super().__init__()
 
-        self.model = torch.hub.load("pytorch/vision:v0.10.0", "squeezenet1_1", pretrained=True)
+        self.model = torch.hub.load(
+            "pytorch/vision:v0.10.0", "squeezenet1_1", pretrained=pretrained
+        )
+
+        self.preprocessed = preprocessed
 
         # self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
 
     def forward(self, x):
+
+        if self.preprocessed:
+            x = self.preprocess(x)
+
         return self.model(x)
 
+    def preprocess(self, x):
+        """Image preprocessing function.
 
-class LitModel(pl.LightningModule):
+        To make image preprocessing model specific and modular.
+
+        Args:
+            x (Any): input image.
+
+        Returns:
+            Any: preprocessed image.
+        """
+
+        preprocess = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        return preprocess(x)
+
+
+class EdemaNet(pl.LightningModule):
     """PyTorch Lightning model class.
 
-    A complete model is implemented (includes the backbone, prototype and fully connected layers).
+    A complete model is implemented (includes the encoder, transient, prototype and fully connected
+    layers). The transient layers are required to concatenate the main encoder layers with the
+    prototype layer. The encoder is the variable part of EdemaNet, which is passed as an argument.
 
     Args:
-        pl (_type_): _description_
+        encoder (nn.Module): encoder layers implemented as a distinct class.
     """
 
-    def __init__(self):
+    def __init__(self, encoder: nn.Module):
         super().__init__()
+
+        # encoder
+        self.encoder = encoder
+
+        # prototypes layer (do not make this just a tensor, since it will not be moved
+        # automatically to gpu)
+        self.prototype_layer = nn.Parameter(torch.rand(self.prototype_shape), requires_grad=True)
 
     def forward(self, x):
         pass
