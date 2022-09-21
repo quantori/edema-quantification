651f7690ee74760ca06d4b5805b366627633fe99
gladiolusnomertri <makoveev90@gmail.com>
2022-09-21 19:46:22 +0200

    _make_transient_layers function is added

--- models_edema.py
+++ models_edema.py
@@ -3,6 +3,7 @@
 The description to be filled...
 """
 
+from typing import Any, Tuple
 import torch
 from torch import nn
 import pytorch_lightning as pl
@@ -19,9 +20,16 @@ class SqueezeNet(nn.Module):
 
     Args:
         nn (_type_): _description_
+
+    Returns:
+        torch.nn.Module: SqueezeNet.
     """
 
-    def __init__(self, preprocessed: bool = True, pretrained: bool = True):
+    def __init__(
+        self,
+        preprocessed: bool = True,
+        pretrained: bool = True,
+    ):
         super().__init__()
 
         self.model = torch.hub.load(
@@ -72,17 +80,40 @@ class EdemaNet(pl.LightningModule):
 
     Args:
         encoder (nn.Module): encoder layers implemented as a distinct class.
+        num_classes (int): the number of feature classes.
+        prototype_shape (Tuple): the shape of the prototypes (batch, channels, H, W).
+        transient_layers_type (str): the architecture of the transient layers. If == 'bottleneck',
+                                     the number of channels is adjusted automatically.
+
+    Returns:
+        pl.LightningModule: EdemaNet.
     """
 
-    def __init__(self, encoder: nn.Module):
+    def __init__(
+        self,
+        encoder: nn.Module,
+        num_classes: int,
+        prototype_shape: Tuple,
+        transient_layers_type: str = "bottleneck",
+    ):
         super().__init__()
+
+        self.transient_layers_type = transient_layers_type
+        self.num_prototypes = prototype_shape[0]
 
         # encoder
         self.encoder = encoder
 
+        # transient layers
+        self.transient_layers = self._make_transient_layers(self.encoder)
+
         # prototypes layer (do not make this just a tensor, since it will not be moved
         # automatically to gpu)
-        self.prototype_layer = nn.Parameter(torch.rand(self.prototype_shape), requires_grad=True)
+        self.prototype_layer = nn.Parameter(torch.rand(prototype_shape), requires_grad=True)
+
+        # Last fully connected layer for the classification of edema features. The bias is not used
+        # in the original paper
+        self.last_layer = nn.Linear(self.num_prototypes, num_classes, bias=False)
 
     def forward(self, x):
         pass
@@ -98,3 +129,74 @@ class EdemaNet(pl.LightningModule):
     def configure_optimizers(self):
         pass
         # return torch.optim.Adam(self.parameters(), lr=0.02)
+
+    def _make_transient_layers(self, encoder: torch.nn.Module) -> torch.nn.Sequential:
+        """Returns transient layers.
+
+        Args:
+            encoder (torch.nn.Module): encoder architecture.
+
+        Returns:
+            torch.nn.Sequential: transient layers as the PyTorch Sequential class.
+        """
+
+        first_transient_layer_in_channels = [
+            i for i in encoder.modules() if isinstance(i, nn.Conv2d)
+        ][-1].out_channels
+
+        # automatic adjustment of the transient-layer channels for matching with the prototype
+        # channels. The activation functions of the intermediate and last transient layers are ReLU
+        # and sigmoid, respectively
+        if self.transient_layers_type == "bottleneck":
+            transient_layers = []
+            current_in_channels = first_transient_layer_in_channels
+
+            while (current_in_channels > self.prototype_shape[1]) or (len(transient_layers) == 0):
+                current_out_channels = max(self.prototype_shape[1], (current_in_channels // 2))
+                transient_layers.append(
+                    nn.Conv2d(
+                        in_channels=current_in_channels,
+                        out_channels=current_out_channels,
+                        kernel_size=1,
+                    )
+                )
+                transient_layers.append(nn.ReLU())
+                transient_layers.append(
+                    nn.Conv2d(
+                        in_channels=current_out_channels,
+                        out_channels=current_out_channels,
+                        kernel_size=1,
+                    )
+                )
+
+                if current_out_channels > self.prototype_shape[1]:
+                    transient_layers.append(nn.ReLU())
+
+                else:
+                    assert current_out_channels == self.prototype_shape[1]
+                    transient_layers.append(nn.Sigmoid())
+
+                current_in_channels = current_in_channels // 2
+
+            transient_layers = nn.Sequential(*transient_layers)
+
+            return transient_layers
+
+        # determined transient layers
+        else:
+            transient_layers = nn.Sequential(
+                nn.Conv2d(
+                    in_channels=first_transient_layer_in_channels,
+                    out_channels=self.prototype_shape[1],
+                    kernel_size=1,
+                ),
+                nn.ReLU(),
+                nn.Conv2d(
+                    in_channels=self.prototype_shape[1],
+                    out_channels=self.prototype_shape[1],
+                    kernel_size=1,
+                ),
+                nn.Sigmoid(),
+            )
+
+            return transient_layers
