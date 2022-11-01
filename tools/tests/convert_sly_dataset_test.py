9d66513c0cfc17858ffbc62a675f936c39051abb
irina.ryndova <irina.ryndova@quantori.com>
2022-11-01 17:42:19 +0300

    add more tests

--- tools/tests/convert_sly_dataset_test.py
+++ tools/tests/convert_sly_dataset_test.py
@@ -26,46 +26,6 @@ ann_test_ok = {
     'size': {'height': 2000, 'width': 3328},
     'objects': [
         {
-            'id': 1066226547,
-            'classId': 10416423,
-            'description': '',
-            'geometryType': 'line',
-            'labelerLogin': 'RenataS',
-            'createdAt': '2022-10-17T12:47:37.167Z',
-            'updatedAt': '2022-10-23T16:44:41.725Z',
-            'tags': [
-                {
-                    'id': 158526170,
-                    'tagId': 30540784,
-                    'name': 'RP',
-                    'value': '3',
-                    'labelerLogin': 'RenataS',
-                    'createdAt': '2022-10-17T12:49:03.635Z',
-                    'updatedAt': '2022-10-17T12:49:03.635Z',
-                }
-            ],
-            'classTitle': 'Kerley',
-            'points': {
-                'exterior': [[237, 1018], [298, 1009], [262, 1016]],
-                'interior': [],
-            },
-        },
-        {
-            'id': 1066226974,
-            'classId': 10419435,
-            'description': '',
-            'geometryType': 'bitmap',
-            'labelerLogin': 'RenataS',
-            'createdAt': '2022-10-17T12:53:12.774Z',
-            'updatedAt': '2022-10-23T16:44:41.725Z',
-            'tags': [],
-            'classTitle': 'Bronchus',
-            'bitmap': {
-                'data': 'eJzrDPBz5+WS4mJgYOD19HAJAtJCIMzBBiTDzvWFAynG4iB3J4Z152ReAjkZni6OIRb+Z6c4cjEocLDc/m9/y88to/tq2YnFLww6usJaZt6MYWJYPS21SG+Ko6CA7iJJ00mOog0zVZUlS7g80uZNcNQ64PqB98R3TzOpBbcTL5+1mxtaqSRa8PvX/MYbZ2acvKH87XrZ/wnfyxPbPZi/Cq17BbSTwdPVz2WdU0ITAASRP5U=',
-                'origin': [479, 615],
-            },
-        },
-        {
             'id': 1066227065,
             'classId': 10421845,
             'description': '',
@@ -76,44 +36,6 @@ ann_test_ok = {
             'tags': [],
             'classTitle': 'Heart',
             'points': {'exterior': [[485, 915], [1260, 985]], 'interior': []},
-        },
-        {
-            'id': 1066227212,
-            'classId': 10416424,
-            'description': '',
-            'geometryType': 'polygon',
-            'labelerLogin': 'RenataS',
-            'createdAt': '2022-10-17T12:56:08.836Z',
-            'updatedAt': '2022-10-23T16:44:41.725Z',
-            'tags': [
-                {
-                    'id': 158526260,
-                    'tagId': 30540784,
-                    'name': 'RP',
-                    'value': '3',
-                    'labelerLogin': 'RenataS',
-                    'createdAt': '2022-10-17T12:58:01.501Z',
-                    'updatedAt': '2022-10-17T12:58:01.501Z',
-                }
-            ],
-            'classTitle': 'Bat',
-            'points': {
-                'exterior': [
-                    [1042, 533],
-                    [1065, 465],
-                    [1082, 411],
-                    [1147, 443],
-                    [1358, 997],
-                    [1362, 1030],
-                    [1333, 1034],
-                    [1271, 1028],
-                    [1255, 997],
-                    [1065, 683],
-                    [1047, 642],
-                    [1034, 579],
-                ],
-                'interior': [],
-            },
         },
     ],
 }
@@ -183,6 +105,96 @@ object_test_no_tags = {
     },
 }
 
+object_test_polyline = {
+    'id': 1066226547,
+    'classId': 10416423,
+    'description': '',
+    'geometryType': 'line',
+    'labelerLogin': 'RenataS',
+    'createdAt': '2022-10-17T12:47:37.167Z',
+    'updatedAt': '2022-10-23T16:44:41.725Z',
+    'tags': [
+        {
+            'id': 158526170,
+            'tagId': 30540784,
+            'name': 'RP',
+            'value': '3',
+            'labelerLogin': 'RenataS',
+            'createdAt': '2022-10-17T12:49:03.635Z',
+            'updatedAt': '2022-10-17T12:49:03.635Z',
+        }
+    ],
+    'classTitle': 'Kerley',
+    'points': {
+        'exterior': [[237, 1018], [298, 1009], [262, 1016]],
+        'interior': [],
+    },
+}
+
+object_test_polygon = {
+    'id': 1066227212,
+    'classId': 10416424,
+    'description': '',
+    'geometryType': 'polygon',
+    'labelerLogin': 'RenataS',
+    'createdAt': '2022-10-17T12:56:08.836Z',
+    'updatedAt': '2022-10-23T16:44:41.725Z',
+    'tags': [
+        {
+            'id': 158526260,
+            'tagId': 30540784,
+            'name': 'RP',
+            'value': '3',
+            'labelerLogin': 'RenataS',
+            'createdAt': '2022-10-17T12:58:01.501Z',
+            'updatedAt': '2022-10-17T12:58:01.501Z',
+        }
+    ],
+    'classTitle': 'Bat',
+    'points': {
+        'exterior': [
+            [1042, 533],
+            [1065, 465],
+            [1358, 997],
+            [1362, 1030],
+            [1271, 1028],
+            [1255, 997],
+            [1047, 642],
+            [1034, 579],
+        ],
+        'interior': [],
+    },
+}
+
+object_test_bitmap = {
+    'id': 1066226974,
+    'classId': 10419435,
+    'description': '',
+    'geometryType': 'bitmap',
+    'labelerLogin': 'RenataS',
+    'createdAt': '2022-10-17T12:53:12.774Z',
+    'updatedAt': '2022-10-23T16:44:41.725Z',
+    'tags': [],
+    'classTitle': 'Bronchus',
+    'bitmap': {
+        'data': 'eJzrDPBz5+WS4mJgYOD19HAJAtJCIMzBBiTDzvWFAynG4iB3J4Z152ReAjkZni6OIRb+Z6c4cjEocLDc/m9/y88to/tq2YnFLww6usJaZt6MYWJYPS21SG+Ko6CA7iJJ00mOog0zVZUlS7g80uZNcNQ64PqB98R3TzOpBbcTL5+1mxtaqSRa8PvX/MYbZ2acvKH87XrZ/wnfyxPbPZi/Cq17BbSTwdPVz2WdU0ITAASRP5U=',
+        'origin': [479, 615],
+    },
+}
+
+object_test_rectangle = {
+    'id': 1066227065,
+    'classId': 10421845,
+    'description': '',
+    'geometryType': 'rectangle',
+    'labelerLogin': 'RenataS',
+    'createdAt': '2022-10-17T12:54:07.876Z',
+    'updatedAt': '2022-10-23T16:44:41.725Z',
+    'tags': [],
+    'classTitle': 'Heart',
+    'points': {'exterior': [[485, 915], [1260, 985]], 'interior': []},
+}
+
 
 def test_dataset_dir_exist():
     check_dataset_dirs(dataset_dir_test, dataset_ann_dir_test, dataset_img_dir_test)
@@ -246,6 +258,19 @@ def test_get_object_rp(caplog):
 
     get_object_rp(object_test_no_tags)
     assert 'There is info about RP!' in caplog.text
+
+
+def test_get_object_sizes():
+    assert get_object_sizes(object_test_polyline) == {'x1': 237, 'y1': 1009, 'x2': 298, 'y2': 1018}
+    assert get_object_sizes(object_test_polygon) == {'x1': 1034, 'y1': 465, 'x2': 1362, 'y2': 1030}
+    assert get_object_sizes(object_test_bitmap) == {'x1': 479, 'y1': 615, 'x2': 497, 'y2': 633}
+    assert get_object_sizes(object_test_rectangle) == {'x1': 485, 'y1': 915, 'x2': 1260, 'y2': 985}
+
+
+def test_get_box_sizes():
+    assert get_box_sizes(0, 0, 0, 0) == {'xc': 0, 'yc': 0, 'box width': 0, 'box height': 0}
+    assert get_box_sizes(0, 0, 1, 1) == {'xc': 0.5, 'yc': 0.5, 'box width': 1, 'box height': 1}
+    assert get_box_sizes(1, 0, 9, 8) == {'xc': 4, 'yc': 4, 'box width': 8, 'box height': 8}
 
 
 def test_metadata():
