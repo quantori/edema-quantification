d6cb8ebf3e2b47419901d8d8e4f21eb7b1d1ed8b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-11-02 19:22:17 +0300

    [WiP]: change the structure of the pipeline

--- tools/convert_sly_dataset.py
+++ tools/convert_sly_dataset.py
@@ -1,12 +1,22 @@
 import os
 import logging
 import argparse
-from pathlib import Path
-import pandas as pd
 from PIL import Image
+from typing import List
+from pathlib import Path
+
+import pandas as pd
 import supervisely_lib as sly
 
-from tools.utils import convert_base64_to_image
+from tools.utils_sly import (
+    CLASS_MAP,
+    FIGURE_MAP,
+    read_sly_project,
+    get_class_name,
+    get_tag_value,
+    get_object_box,
+    get_box_sizes,
+)
 
 os.makedirs('logs', exist_ok=True)
 logging.basicConfig(
@@ -19,30 +29,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def check_dataset_dirs(dataset_dir: str, dataset_ann_dir: str, dataset_img_dir: str) -> None:
-    """
-
-    Args:
-        dataset_dir: a path to the MIMIC-CXR dataset
-        dataset_ann_dir: a path to the MIMIC-CXR dataset annotations
-        dataset_img_dir: a path to the MIMIC-CXR dataset images
-
-    Returns:
-        None
-    """
-    logger.info('Checking dataset paths')
-
-    if not os.path.isdir(dataset_dir):
-        raise OSError(f'{dataset_dir} does not exist!')
-
-    if not os.path.isdir(dataset_ann_dir):
-        raise OSError(f'{dataset_ann_dir} does not exist!')
-
-    if not os.path.isdir(dataset_img_dir):
-        raise OSError(f'{dataset_img_dir} does not exist!')
-
-
-def crop_images(dataset_img_dir: str, save_dir: str) -> None:
+# TODO: I would suggest processing images along with annotations
+def crop_images(
+        dataset_img_dir: str,
+        save_dir: str,
+) -> None:
     """
 
     Args:
@@ -63,109 +54,20 @@ def crop_images(dataset_img_dir: str, save_dir: str) -> None:
     left = 0
     for img_name in img_list:
         img = Image.open(os.path.join(dataset_img_dir, img_name))
-        subject_id, study_id, left_width, right_width, ext = img_name.replace('.', '_').split('_')
+        subject_id, study_id, left_width, right_width, ext = img_name.replace(
+            '.', '_'
+        ).split('_')
         right = int(left_width)
         bottom = img.height
 
         img = img.crop((left, top, right, bottom))
-
         img.save(os.path.join(save_img_dir, f'{subject_id}_{study_id}.{ext}'))
 
 
-def get_edema_name(ann: dict) -> str:
-    """
-
-    Args:
-        ann: dictionary with supervisely annotations
-
-    Returns:
-        string with edema name
-    """
-    if ann['tags']:
-        check_labeler_login(ann['tags'][0]['labelerLogin'])
-        edema_name = ann['tags'][0]['value']
-    else:
-        logger.warning('There is no tags!')
-        edema_name = ''
-    return edema_name
-
-
-def check_labeler_login(labeler_login: str) -> None:
-    """
-
-    Args:
-        labeler_login: string from supervisely annotations which contains a labeler login
-
-    Returns:
-        None
-    """
-    if labeler_login in ('ViacheslavDanilov', 'mak_en', 'irina.ryndova'):
-        logger.error(f'Wrong labeler login: {labeler_login}')
-
-
-def get_object_rp(obj: dict) -> str:
-    """
-
-    Args:
-        obj: dictionary with information about one object from supervisely annotations
-
-    Returns:
-        string with representativeness level
-    """
-    if obj['tags']:
-        rp = obj['tags'][0]['value']
-    else:
-        logger.warning('There is info about RP!')
-        rp = ''
-    return rp
-
-
-def get_object_sizes(obj: dict) -> dict:
-    """
-
-    Args:
-        obj: dictionary with information about one object from supervisely annotations
-
-    Returns:
-        dictionary which contains coordinates for a rectangle (left, top, right, bottom)
-    """
-    if obj['geometryType'] == 'bitmap':
-        bitmap = convert_base64_to_image(obj['bitmap']['data'])
-        x1, y1 = obj['bitmap']['origin'][0], obj['bitmap']['origin'][1]
-        x2 = x1 + bitmap.shape[0]
-        y2 = y1 + bitmap.shape[1]
-    else:
-        xs = [x[0] for x in obj['points']['exterior']]
-        ys = [x[1] for x in obj['points']['exterior']]
-        x1, y1, x2, y2 = min(xs), min(ys), max(xs), max(ys)
-
-    return {
-        'x1': x1,
-        'y1': y1,
-        'x2': x2,
-        'y2': y2,
-    }
-
-
-def get_box_sizes(x1: int, y1: int, x2: int, y2: int) -> dict:
-    """
-
-    Args:
-        x1: left x
-        y1: top y
-        x2: right x
-        y2: bottom y
-
-    Returns:
-        dictionary which contains coordinates for rectangle (a center point and a width/height)
-    """
-    box_width, box_height = x2 - x1, y2 - y1
-    xc, yc = box_width / 2, box_height / 2
-
-    return {'xc': xc, 'yc': yc, 'box width': box_width, 'box height': box_height}
-
-
-def prepare_metadata_annotations(dataset_ann_dir: str, save_dir: str) -> None:
+def prepare_metadata_annotations(
+        dataset_ann_dir: str,
+        save_dir: str,
+) -> None:
     """
 
     Args:
@@ -180,57 +82,48 @@ def prepare_metadata_annotations(dataset_ann_dir: str, save_dir: str) -> None:
     save_ann_dir = os.path.join(save_dir, 'ann')
     os.makedirs(save_ann_dir, exist_ok=True)
 
-    edema_id = {
-        '': 0,
-        'Vascular congestion': 1,
-        'Interstitial edema': 2,
-        'Alveolar edema': 3,
-    }
-
-    figure_id = {
-        'Cephalization': 0,
-        'Artery': 1,
-        'Heart': 2,
-        'Kerley': 3,
-        'Bronchus': 4,
-        'Effusion': 5,
-        'Bat': 6,
-        'Infiltrate': 7,
-    }
-
     metadata = pd.DataFrame(
         columns=[
-            'image path',
-            'subject id',
-            'study id',
-            'image width',
-            'image height',
-            'figure',
+            'Image path',
+            'Subject ID',
+            'Study id',
+            'Image width',
+            'Image height',
+            'Figure',
             'x1',
             'y1',
             'x2',
             'y2',
             'xc',
             'yc',
-            'box width',
-            'box height',
-            'rp',
-            'class',
+            'Box width',
+            'Box height',
+            'RP',
+            # 'Mask',           # TODO: add mask string
+            # 'Points',         # TODO: add mask string
+            # 'Class ID',       # TODO: add class id
+            'Class',
         ]
     )
 
     ann_list = os.listdir(dataset_ann_dir)
     for ann_name in ann_list:
         logger.info(f'Processing annotation {ann_name}')
-        annotation = pd.DataFrame(columns=['edema id', 'figure id', 'x1', 'y1', 'x2', 'y2'])
+        annotation = pd.DataFrame(
+            columns=['edema id', 'figure id', 'x1', 'y1', 'x2', 'y2']
+        )
 
         ann = sly.io.json.load_json_file(os.path.join(dataset_ann_dir, ann_name))
 
-        edema_name = get_edema_name(ann)
+        class_name = get_class_name(ann)
 
-        subject_id, study_id, width, right_width, ext, _ = ann_name.replace('.', '_').split('_')
+        subject_id, study_id, width, right_width, ext, _ = ann_name.replace(
+            '.', '_'
+        ).split('_')
         height = ann['size']['height']
-        cropped_img_path = os.path.join(save_dir, 'img', f'{subject_id}_{study_id}.{ext}')
+        cropped_img_path = os.path.join(
+            save_dir, 'img', f'{subject_id}_{study_id}.{ext}'
+        )
 
         if len(ann['objects']) == 0:
             logger.warning(f'There is no objects!')
@@ -238,29 +131,28 @@ def prepare_metadata_annotations(dataset_ann_dir: str, save_dir: str) -> None:
 
         for obj in ann['objects']:
             logger.info(f'Processing object {obj}')
-            check_labeler_login(obj['labelerLogin'])
 
-            rp = get_object_rp(obj)
-            xy = get_object_sizes(obj)
+            # rp = get_tag_value(obj, tag_name='RP')             # TODO: implement extraction tag_value by tag_name (in our case 'RP'), uncomment when fixed
+            xy = get_object_box(obj)
             box = get_box_sizes(*xy.values())
             figure_name = obj['classTitle']
 
             annotation_info = {
-                'edema id': edema_id[edema_name],
-                'figure id': figure_id[figure_name],
+                'edema id': CLASS_MAP[class_name],
+                'figure id': FIGURE_MAP[figure_name],
             }
             annotation_info.update(xy)
             annotation = annotation.append(annotation_info, ignore_index=True)
 
             image_info = {
-                'image path': cropped_img_path,
-                'subject id': subject_id,
-                'study id': study_id,
-                'image width': width,
-                'image height': height,
-                'figure': figure_name,
-                'rp': rp,
-                'class': edema_name,
+                'Image path': cropped_img_path,
+                'Subject ID': subject_id,
+                'Study id': study_id,
+                'Image width': width,
+                'Image height': height,
+                'Figure': figure_name,
+                'RP': rp,
+                'Class': class_name,
             }
             image_info.update(xy)
             image_info.update(box)
@@ -275,40 +167,73 @@ def prepare_metadata_annotations(dataset_ann_dir: str, save_dir: str) -> None:
             sep=' ',
         )
 
+    # TODO: create a dataframe and save metadata as an XLSX file
+    # df.index += 1
+    # df.to_excel(
+    #     save_path,
+    #     sheet_name='Metadata',
+    #     index=True,
+    #     index_label='ID',
+    # )
     logging.info('Saving metadata.csv')
     metadata.to_csv(os.path.join(save_dir, f'metadata.csv'))
 
 
 def main(
-    dataset_dir: str,
-    save_dir: str,
+        dataset_dir: str,
+        include_dirs: List[str],
+        exclude_dirs: List[str],
+        save_dir: str,
 ) -> None:
     """
 
     Args:
-        dataset_dir: a path to the MIMIC-CXR dataset
+        dataset_dir: a path to Supervisely dataset directory
+        include_dirs: a list of subsets to include in the dataset
+        exclude_dirs: a list of subsets to exclude from the dataset
         save_dir: directory where the output files will be saved
 
     Returns:
         None
     """
-    dataset_ann_dir = os.path.join(dataset_dir, 'ann')
-    dataset_img_dir = os.path.join(dataset_dir, 'img')
+    # TODO: remove
+    dataset_ann_dir = os.path.join(dataset_dir, 'DS1', 'ann')
+    dataset_img_dir = os.path.join(dataset_dir, 'DS1', 'img')
 
-    check_dataset_dirs(dataset_dir, dataset_ann_dir, dataset_img_dir)
+    # TODO (@irina.ryndova): remove it as SLY automatically checks the structure of the dataset implemented in read_sly_project
+    # check_dataset_dirs(dataset_dir, dataset_ann_dir, dataset_img_dir)
 
-    crop_images(dataset_img_dir, save_dir)
+    df = read_sly_project(
+        dataset_dir=dataset_dir,
+        include_dirs=include_dirs,
+        exclude_dirs=exclude_dirs,
+    )
+
+    # TODO: process images and annotations together
+    # TODO: Allocate dataframes/dicts per each image
+    # for idx, row in df.iterrows():
+    #     print(row)
+    #     # process_image(...)
+
+    # TODO: Create final dataframe
+    # TODO (@irina.ryndova): parallel processing (optional)
+    # crop_images(dataset_img_dir, save_dir)
 
     prepare_metadata_annotations(dataset_ann_dir, save_dir)
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser(description='Convert Supervisely dataset')
-    parser.add_argument('--dataset_dir', default='dataset/MIMIC-CXR-Edema-SLY/DS1', type=str)
+    parser.add_argument('--dataset_dir', default='dataset/MIMIC-CXR-Edema-SLY', type=str)
+    parser.add_argument('--include_dirs', nargs='+', default=None, type=str)
+    parser.add_argument('--exclude_dirs', nargs='+', default=None, type=str)
     parser.add_argument('--save_dir', default='dataset/MIMIC-CXR-Edema-Convert', type=str)
     args = parser.parse_args()
 
     main(
         dataset_dir=args.dataset_dir,
+        include_dirs=args.include_dirs,
+        exclude_dirs=args.exclude_dirs,
         save_dir=args.save_dir,
     )
