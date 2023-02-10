b8705de67b0c5cead9dcbf720c8fdcc64c10bc08
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-02-10 19:41:14 +0100

    [Refactor]: use new project structure

--- src/data/utils_coco.py
+++ src/data/utils_coco.py
@@ -1,17 +1,17 @@
 import os
-from typing import Dict, List, Tuple, Any
+from typing import Any, Dict, List, Tuple, Union
 
 import cv2
 import pandas as pd
 
-from tools.utils_sly import ANNOTATION_COLUMNS, FIGURE_MAP_REVERSED
+from src.data.utils_sly import ANNOTATION_COLUMNS, FIGURE_MAP_REVERSED
 
 
 def get_img_info(
     img_path: str,
     img_id: int,
 ) -> Dict[str, Any]:
-    img_data = {}
+    img_data: Dict[str, Union[int, str]] = {}
     height, width = cv2.imread(img_path).shape[:2]
     img_data['id'] = img_id  # Unique image ID
     img_data['width'] = width
@@ -30,7 +30,7 @@ def get_ann_info(
     if os.path.exists(label_path):
         df_ann = pd.read_csv(label_path, sep='\t', names=ANNOTATION_COLUMNS)
         for _, row in df_ann.iterrows():
-            label = {}
+            label: Dict[str, Union[int, List[int]]] = {}
             if row['Class ID'] > 0:
                 box_extension_figure = box_extension[FIGURE_MAP_REVERSED[row['Figure ID']]]
                 x1, y1 = (
@@ -39,13 +39,13 @@ def get_ann_info(
                 )
                 x2, y2 = (
                     int(row['x2']) + box_extension_figure[0],
-                    int(row['y2']) + box_extension_figure[1]
+                    int(row['y2']) + box_extension_figure[1],
                 )
                 width = abs(x2 - x1 + 1)
                 height = abs(y2 - y1 + 1)
 
-                label['id'] = ann_id            # Should be unique
-                label['image_id'] = img_id      # Image ID annotation relates to
+                label['id'] = ann_id  # Should be unique
+                label['image_id'] = img_id  # Image ID annotation relates to
                 label['category_id'] = int(row['Figure ID'])
                 label['bbox'] = [x1, y1, width, height]
                 label['area'] = width * height
