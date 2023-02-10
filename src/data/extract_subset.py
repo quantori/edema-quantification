66a060bed9ee1e22f2aea617f94a7a178d150fa6
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-02-10 19:15:34 +0100

    [Refactor]: use new project structure

--- src/data/extract_subset.py
+++ src/data/extract_subset.py
@@ -1,13 +1,13 @@
+import argparse
+import logging
 import os
 import shutil
-import logging
-import argparse
+from functools import partial
 from pathlib import Path
 from typing import Tuple
-from functools import partial
-from joblib import Parallel, delayed
 
 import pandas as pd
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
 os.makedirs('logs', exist_ok=True)
@@ -22,10 +22,10 @@ logger = logging.getLogger(__name__)
 
 
 def process_single_study(
-        group: Tuple[int, pd.DataFrame],
-        dataset_dir: str,
-        save_pairs_only: bool,
-        save_dir: str,
+    group: Tuple[int, pd.DataFrame],
+    dataset_dir: str,
+    save_pairs_only: bool,
+    save_dir: str,
 ) -> pd.DataFrame:
     _, df_study = group
     df_study.reset_index(drop=True, inplace=True)
@@ -35,16 +35,19 @@ def process_single_study(
     # Process study with paired images
     study_views = [df_study.iloc[idx]['View'] for idx in range(len(df_study))]
     is_correct_view = (
-            ('AP' in study_views) and ('LATERAL' in study_views)
-            or ('PA' in study_views) and ('LATERAL' in study_views)
-            or ('AP' in study_views) and ('LL' in study_views)
-            or ('PA' in study_views) and ('LL' in study_views)
+        ('AP' in study_views)
+        and ('LATERAL' in study_views)
+        or ('PA' in study_views)
+        and ('LATERAL' in study_views)
+        or ('AP' in study_views)
+        and ('LL' in study_views)
+        or ('PA' in study_views)
+        and ('LL' in study_views)
     )
 
     # Save paired images of a study
     study_dir = os.path.join(save_dir, 'files', f'{subject_id}', f'{study_id}')
     if save_pairs_only:
-
         if is_correct_view and len(df_study) == 2:
             os.makedirs(study_dir, exist_ok=True)
             for idx, _src_path in df_study['Image path'].iteritems():
@@ -81,24 +84,21 @@ def process_single_study(
 
 
 def extract_subset(
-        dataset_dir: str,
-        finding: str,
-        save_pairs_only: bool,
-        save_dir: str,
+    dataset_dir: str,
+    finding: str,
+    save_pairs_only: bool,
+    save_dir: str,
 ) -> None:
-
-    """
+    """Extract a subset with a specified disease from the MIMIC dataset.
 
     Args:
         dataset_dir: a path to the MIMIC-CXR dataset
         finding: one of the findings to be extracted
         save_pairs_only: if True, save only the paired cases (frontal and lateral images)
         save_dir: directory where the output files will be saved
-
     Returns:
         None
     """
-
     logger.info(f'Dataset dir...............: {dataset_dir}')
     logger.info(f'Finding...................: {finding}')
     logger.info(f'Save pairs only...........: {save_pairs_only}')
@@ -118,7 +118,8 @@ def extract_subset(
         save_dir=save_dir,
     )
     result = Parallel(n_jobs=-1)(
-        delayed(processing_func)(group) for group in tqdm(groups, desc='Dataset conversion', unit=' study')
+        delayed(processing_func)(group)
+        for group in tqdm(groups, desc='Dataset conversion', unit=' study')
     )
     df_out = pd.concat(result)
     df_out.reset_index(drop=True, inplace=True)
@@ -138,7 +139,6 @@ def extract_subset(
 
 
 if __name__ == '__main__':
-
     DISEASES = [
         'Atelectasis',
         'Cardiomegaly',
@@ -157,10 +157,10 @@ if __name__ == '__main__':
     ]
 
     parser = argparse.ArgumentParser(description='Extract MIMIC subset')
-    parser.add_argument('--dataset_dir', default='dataset/MIMIC-CXR', type=str)
+    parser.add_argument('--dataset_dir', default='data/raw', type=str)
     parser.add_argument('--disease', default='Edema', type=str, choices=DISEASES)
     parser.add_argument('--save_pairs_only', action='store_true')
-    parser.add_argument('--save_dir', default='dataset/MIMIC-CXR-Edema', type=str)
+    parser.add_argument('--save_dir', default='data/edema', type=str)
     args = parser.parse_args()
 
     extract_subset(
