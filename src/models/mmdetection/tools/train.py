b36b9761546d5b855664b62c974cdc1946f1c8ec
irina.ryndova <irina.ryndova@quantori.com>
2023-03-24 17:11:27 +0400

    fix config exceptions

--- src/models/mmdetection/tools/train.py
+++ src/models/mmdetection/tools/train.py
@@ -196,23 +196,23 @@ def main():
             cfg.model.roi_head.bbox_head.num_classes = len(CLASSES)
         except Exception:
             try:
-                cfg.model.roi_head.mask_head.num_classes = len(CLASSES)
+                for bbox_head in cfg.model.bbox_head:
+                    bbox_head.num_classes = len(CLASSES)
+                for roi_head_mask_head in cfg.model.roi_head.mask_head:
+                    roi_head_mask_head.num_classes = len(CLASSES)
                 for roi_head_bbox_head in cfg.model.roi_head.bbox_head:
                     roi_head_bbox_head.num_classes = len(CLASSES)
             except Exception:
                 try:
-                    for bbox_head in cfg.model.bbox_head:
-                        bbox_head.num_classes = len(CLASSES)
                     for roi_head_mask_head in cfg.model.roi_head.mask_head:
                         roi_head_mask_head.num_classes = len(CLASSES)
-                    for roi_head_bbox_head in cfg.model.roi_head.bbox_head:
-                        roi_head_bbox_head.num_classes = len(CLASSES)
+                    for head in cfg.model.roi_head.bbox_head:
+                        head.num_classes = len(CLASSES)
                 except Exception:
                     try:
-                        for roi_head_mask_head in cfg.model.roi_head.mask_head:
-                            roi_head_mask_head.num_classes = len(CLASSES)
-                        for head in cfg.model.roi_head.bbox_head:
-                            head.num_classes = len(CLASSES)
+                        cfg.model.roi_head.mask_head.num_classes = len(CLASSES)
+                        for roi_head_bbox_head in cfg.model.roi_head.bbox_head:
+                            roi_head_bbox_head.num_classes = len(CLASSES)
                     except Exception as ex:
                         print(f'\n\n\nError: {ex}')
 
