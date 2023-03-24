e928b33771bc31f08aaebde6c3515043d8a0f67d
irina.ryndova <irina.ryndova@quantori.com>
2023-03-24 16:42:46 +0400

    add another case for configs

--- src/models/mmdetection/tools/train.py
+++ src/models/mmdetection/tools/train.py
@@ -197,10 +197,24 @@ def main():
         except Exception:
             try:
                 cfg.model.roi_head.mask_head.num_classes = len(CLASSES)
-                for head in cfg.model.roi_head.bbox_head:
-                    head.num_classes = len(CLASSES)
-            except Exception as ex:
-                print(f'\n\n\nError: {ex}')
+                for roi_head_bbox_head in cfg.model.roi_head.bbox_head:
+                    roi_head_bbox_head.num_classes = len(CLASSES)
+            except Exception:
+                try:
+                    for bbox_head in cfg.model.bbox_head:
+                        bbox_head.num_classes = len(CLASSES)
+                    for roi_head_mask_head in cfg.model.roi_head.mask_head:
+                        roi_head_mask_head.num_classes = len(CLASSES)
+                    for roi_head_bbox_head in cfg.model.roi_head.bbox_head:
+                        roi_head_bbox_head.num_classes = len(CLASSES)
+                except Exception:
+                    try:
+                        for roi_head_mask_head in cfg.model.roi_head.mask_head:
+                            roi_head_mask_head.num_classes = len(CLASSES)
+                        for head in cfg.model.roi_head.bbox_head:
+                            head.num_classes = len(CLASSES)
+                    except Exception as ex:
+                        print(f'\n\n\nError: {ex}')
 
     cfg.data.train.type = args.dataset_type
     cfg.data.train.classes = cfg.classes
