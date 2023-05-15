46cc6e2fdf8a1f54f0eecafcbcac01fc054415d9
irina.ryndova <irina.ryndova@quantori.com>
2023-05-15 11:53:26 +0300

    feat: add center crop and distortion

--- src/models/mmdetection/configs/vfnet/vfnet_r50_fpn_mstrain_2x_coco.py
+++ src/models/mmdetection/configs/vfnet/vfnet_r50_fpn_mstrain_2x_coco.py
@@ -13,7 +13,10 @@ train_pipeline = [
         multiscale_mode='range',
         keep_ratio=True,
     ),
-    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomCenterCropPad'),
+    dict(type='RandomFlip', flip_ratio=0.5, direction='horizontal'),
+    dict(type='PhotoMetricDistortion', hue_delta=0),
+
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
