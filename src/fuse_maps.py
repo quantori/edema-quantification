f700d91f9e2585ac3b5139d7999402a05289c40c
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-05-26 22:28:26 +0300

    [Feature]: implement the fusion pipeline

--- src/fuse_maps.py
+++ src/fuse_maps.py
@@ -6,12 +6,19 @@ from typing import List
 import cv2
 import hydra
 import numpy as np
+import pandas as pd
 from joblib import Parallel, delayed
 from omegaconf import DictConfig, OmegaConf
 from tqdm import tqdm
 
 from src.data.utils import get_file_list
-from src.data.utils_sly import FEATURE_MAP, FEATURE_TYPE, get_box_sizes
+from src.data.utils_sly import (
+    FEATURE_MAP,
+    FEATURE_TYPE,
+    METADATA_COLUMNS,
+    convert_mask_to_base64,
+    get_box_sizes,
+)
 from src.models.map_fuser import MapFuser
 from src.models.mask_processor import MaskProcessor
 
@@ -34,6 +41,9 @@ def compute_lungs_info(
     x1, y1 = min(x1_values), min(y1_values)
     x2, y2 = max(x2_values), max(y2_values)
 
+    mask_crop = mask[y1:y2, x1:x2]
+    mask_encoded = convert_mask_to_base64(mask_crop)
+
     feature_name = 'Lungs'
     lungs_info = {
         'Feature ID': FEATURE_MAP[feature_name],
@@ -43,6 +53,7 @@ def compute_lungs_info(
         'y1': y1,
         'x2': x2,
         'y2': y2,
+        'Mask': mask_encoded,
     }
 
     lungs_info.update(get_box_sizes(x1=x1, y1=y1, x2=x2, y2=y2))
@@ -79,7 +90,21 @@ def process_prob_maps(
     cv2.imwrite(mask_path, mask_clean)
 
     # Extract lungs metadata
-    lungs_info = compute_lungs_info(mask=mask_clean)
+    img_stem = Path(img_path).stem
+    subject_id, study_id = img_stem.split('_')
+    map_height, map_width = fused_map.shape[:2]
+    map_ratio = map_height / map_width
+    lungs_info = {
+        'Image name': img_name,
+        'Subject ID': subject_id,
+        'Study ID': study_id,
+        'Image width': map_width,
+        'Image height': map_height,
+        'Image ratio': map_ratio,
+        'View': 'Frontal',
+    }
+    lung_coords = compute_lungs_info(mask=mask_clean)
+    lungs_info.update(lung_coords)
 
     return lungs_info
 
@@ -130,107 +155,36 @@ def main(cfg: DictConfig) -> None:
                 '.bmp',
             ],
         )
-        img_paths_ = img_paths_[:10]  # TODO: remove after debugging
-        # img_path_sets.append(img_paths_)
         img_path_sets.append(img_paths_)
-        log.info(f'{model_name}...............: {len(img_paths_)} images')
+        log.info(f'{len(img_paths_)} images are used for {model_name}')
 
     # Reorder image paths for multiprocessing
     img_path_sets = reorder_image_paths(img_path_sets)
 
     # Process segmentation probability maps
-    Parallel(n_jobs=-1)(
+    lung_info = Parallel(n_jobs=-1)(
         delayed(process_prob_maps)(img_path_set, cfg.save_dir)
         for img_path_set in tqdm(img_path_sets, desc='Processing')
     )
 
-    # TODO: remove after debugging
-    # Process probability map
-    # from src.models.mask_processor import MaskProcessor
-    #
-    # processor = MaskProcessor()
-    # mask_bin = processor.binarize_image(image=fused_map)
-    # mask_smooth = processor.smooth_mask(mask=mask_bin)
-    # mask_clean = processor.remove_artifacts(mask=mask_smooth)
-    # lungs_info = compute_lungs_info(mask=mask_clean)
-    # for model_name in cfg.model_names:
-    #     log.info(f'Model in use: {model_name}')
-    #     print(f'Model in use: {model_name}')
-    #     model_dir = os.path.join(cfg.model_dirs, model_name)
-    #     map_dir = os.path.join(cfg.save_dir, f'{model_name}', 'map')
-    #     mask_dir = os.path.join(cfg.save_dir, f'{model_name}', 'mask')
-    #     os.makedirs(map_dir, exist_ok=True)
-    #     os.makedirs(mask_dir, exist_ok=True)
-    #
-    #     metadata = pd.DataFrame(columns=METADATA_COLUMNS)
-    #
-    #     model = LungSegmenter(
-    #         model_dir=model_dir,
-    #         device='auto',
-    #     )
-    #
-    #     for img_path in tqdm(img_paths, desc='Lung segmentation', unit='images'):
-    #         img_name = Path(img_path).name
-    #         img = cv2.imread(img_path)
-    #         img_height, img_width = img.shape[:2]
-    #         img_ratio = img_height / img_width
-    #
-    #         # Retrieve and save a probability segmentation map
-    #         map_ = model(
-    #             img=img,
-    #             scale_output=True,
-    #         )
-    #         map = cv2.resize(map_, (img_width, img_height), interpolation=cv2.INTER_LANCZOS4)
-    #         map_path = os.path.join(map_dir, img_name)
-    #         cv2.imwrite(map_path, map)
-    #
-    #         # Retrieve and save a binary segmentation mask
-    #         mask_ = model.binarize_map(
-    #             map=map,
-    #             threshold_method='otsu',
-    #         )
-    #         mask_smooth = model.smooth_mask(mask=mask_)
-    #         mask_lungs = model.remove_artifacts(mask=mask_smooth)
-    #         mask_path = os.path.join(mask_dir, img_name)
-    #         cv2.imwrite(mask_path, mask_lungs)
-    #
-    #         # Compute lung coordinates
-    #         lungs_info = model.compute_lungs_info(mask=mask_lungs)
-    #
-    #         # Extract metadata
-    #         img_stem = Path(img_path).stem
-    #         subject_id, study_id = img_stem.split('_')
-    #         obj_info = {
-    #             'Image path': img_path,
-    #             'Image name': img_name,
-    #             'Subject ID': subject_id,
-    #             'Study ID': study_id,
-    #             'Image width': img_width,
-    #             'Image height': img_height,
-    #             'Image ratio': img_ratio,
-    #             'View': 'Frontal',
-    #         }
-    #         obj_info.update(lungs_info)
-    #         # TODO: add encoded mask to the column "Mask"
-    #         metadata = metadata.append(obj_info, ignore_index=True)
-    #
-    #     # Save metadata
-    #     metadata_path = os.path.join(model_dir, 'metadata.xlsx')
-    #     log.info(f'Saving metadata to {metadata_path}')
-    #     metadata.sort_values(['Image path'], inplace=True)
-    #     metadata.reset_index(drop=True, inplace=True)
-    #     metadata.index += 1
-    #     metadata.to_excel(
-    #         metadata_path,
-    #         sheet_name='Metadata',
-    #         index=True,
-    #         index_label='ID',
-    #     )
-    #
-    #     # Empty memory
-    #     del model
-    #     gc.collect()
-    #     torch.cuda.empty_cache()
+    # Create metadata
+    metadata = pd.DataFrame(columns=METADATA_COLUMNS)
+    metadata = metadata.append(lung_info, ignore_index=True)
+
+    # Save metadata
+    metadata_path = os.path.join(cfg.save_dir, 'metadata.xlsx')
+    log.info(f'Saving metadata to {metadata_path}')
+    metadata.sort_values(['Image name'], inplace=True)
+    metadata.reset_index(drop=True, inplace=True)
+    metadata.index += 1
+    metadata.to_excel(
+        metadata_path,
+        sheet_name='Metadata',
+        index=True,
+        index_label='ID',
+    )
+
+    log.info('Complete')
 
 
 if __name__ == '__main__':
