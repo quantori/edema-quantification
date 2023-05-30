2567b4cbcadd1b314e1a5e315ad744e4387d5990
irina.ryndova <irina.ryndova@quantori.com>
2023-05-30 11:23:15 +0300

    [Feat]: add visualize evaluation config

--- src/vis/visualize_evaluation.py
+++ src/vis/visualize_evaluation.py
@@ -1,46 +1,52 @@
+import logging
+import os
+
 import fiftyone as fo
+import hydra
 from fiftyone import ViewField as F
-import fiftyone.utils.coco as fouc
+from omegaconf import DictConfig, OmegaConf
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
 
-def main():
-    try:
-        dataset = fo.Dataset.from_json(path_or_str='data/coco/test/eval_dataset.json')
-    except ValueError as verr:
-        ...
-    # Evaluate the objects in the `predictions` field with respect to the
-    # objects in the `ground_truth` field
+@hydra.main(
+    config_path=os.path.join(os.getcwd(), 'configs'),
+    config_name='visualize_evaluation',
+    version_base=None,
+)
+def main(cfg: DictConfig) -> None:
+    log.info(f'Config:\n\n{OmegaConf.to_yaml(cfg)}')
+    """Visualize a COCO dataset to verify its correctness.
+
+    Args:
+        eval_dataset_path: a json file with prediction and ground truth data
+        n_most_common_classes: a number of the most common classes to analyse
+    Returns:
+        None
+    """
+    dataset = fo.Dataset.from_json(path_or_str=cfg.eval_dataset_path)
     results = dataset.evaluate_detections(
-        "predictions",
-        gt_field="ground_truth",
-        eval_key="eval",
+        'predictions',
+        gt_field='ground_truth',
+        eval_key='eval',
     )
 
-    # Get the 10 most common classes in the dataset
-    counts = dataset.count_values("ground_truth.detections.label")
-    classes = sorted(counts, key=counts.get, reverse=True)[:10]
-
-    # Print a classification report for the top-10 classes
+    counts = dataset.count_values('ground_truth.detections.label')
+    classes = sorted(counts, key=counts.get, reverse=True)[: cfg.n_most_common_classes]
     results.print_report(classes=classes)
 
-    # Print some statistics about the total TP/FP/FN counts
-    print("TP: %d" % dataset.sum("eval_tp"))
-    print("FP: %d" % dataset.sum("eval_fp"))
-    print("FN: %d" % dataset.sum("eval_fn"))
+    print('TP: %d' % dataset.sum('eval_tp'))
+    print('FP: %d' % dataset.sum('eval_fp'))
+    print('FN: %d' % dataset.sum('eval_fn'))
 
-    # Create a view that has samples with the most false positives first, and
-    # only includes false positive boxes in the `predictions` field
-    view = (
-        dataset
-            .sort_by("eval_fp", reverse=True)
-            .filter_labels("predictions", F("eval") == "fp")
-    )
+    view = dataset.sort_by('eval_fp', reverse=True).filter_labels('predictions', F('eval') == 'fp')
 
-    # Visualize results in the App
     session = fo.launch_app(view=view)
 
     session.wait()
     dataset.delete()
+
 
 if __name__ == '__main__':
     main()
