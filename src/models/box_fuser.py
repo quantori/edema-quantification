96f41d619f9d1fbf2ab07072cd8a22a529fcf2a1
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-07 09:19:04 +0300

    [WIP]: convert box coordinates to source notation

--- src/models/box_fuser.py
+++ src/models/box_fuser.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
 import pandas as pd
 from ensemble_boxes import nms, soft_nms
 from tqdm import tqdm
+
+from src.data.utils_sly import FEATURE_MAP_REVERSED
 
 
 class BoxFuser:
@@ -20,6 +24,7 @@ class BoxFuser:
         self,
         detections: pd.DataFrame,
     ) -> pd.DataFrame:
+        detections.drop(columns=['ID'], inplace=True)
         df_out = pd.DataFrame(columns=detections.columns)
 
         # Process predictions one image at a time
@@ -68,7 +73,46 @@ class BoxFuser:
             else:
                 raise ValueError(f'Unknown method: {self.method}')
 
+            # TODO: move to another function
+            df_img_ = pd.DataFrame(index=range(len(boxes)), columns=detections.columns)
+            img_width = df_img['Image width'].unique()[0]
+            img_height = df_img['Image height'].unique()[0]
+            df_img_['Image path'] = img_path
+            df_img_['Image name'] = Path(img_path).name
+            df_img_['Image height'] = img_height
+            df_img_['Image width'] = img_width
+            df_img_['x1'] = (boxes[:, 0] * img_width).astype(int)
+            df_img_['y1'] = (boxes[:, 1] * img_width).astype(int)
+            df_img_['x2'] = (boxes[:, 2] * img_width).astype(int)
+            df_img_['y2'] = (boxes[:, 3] * img_width).astype(int)
+            df_img_['Box width'] = df_img_.apply(
+                func=lambda row: abs(row['x2'] - row['x1'] + 1),
+                axis=1,
+            )
+            df_img_['Box height'] = df_img_.apply(
+                func=lambda row: abs(row['y2'] - row['y1'] + 1),
+                axis=1,
+            )
+            df_img_['Box area'] = df_img_.apply(
+                func=lambda row: row['Box width'] * row['Box height'],
+                axis=1,
+            )
+            df_img_['Feature ID'] = labels
+            df_img_['Feature'] = df_img_.apply(
+                func=lambda row: FEATURE_MAP_REVERSED[row['Feature ID']],
+                axis=1,
+            )
+            df_img_['Confidence'] = scores
+
         return df_out
+
+    # def process_dataframe(
+    #         self,
+    #         df: pd.DataFrame,
+    # ) -> pd.DataFrame:
+    #
+    #     # Convert boxes back
+    #     return df_out
 
 
 if __name__ == '__main__':
