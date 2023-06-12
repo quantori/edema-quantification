78b42017244ffc0ea2f1ac53c89f4ae7dbe6fa91
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-12 16:59:41 +0300

    [Fix]: remove imports

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models.feature_detector import FeatureDetector  # noqa: F401
-from .models.lung_segmenter import LungSegmenter  # noqa: F401
-from .models.map_fuser import MapFuser  # noqa: F401
-from .models.mask_processor import MaskProcessor  # noqa: F401
