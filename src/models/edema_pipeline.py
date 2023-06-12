58aafe085b325e93b409d4da9eb23a554de9ff51
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-12 21:53:58 +0300

    [Feature]: add the calculation of lung metadata

--- src/models/edema_pipeline.py
+++ src/models/edema_pipeline.py
@@ -4,13 +4,14 @@ import os
 import shutil
 from glob import glob
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
 import torch
 
 from src.data.utils import get_file_list
+from src.data.utils_sly import FEATURE_MAP, get_box_sizes
 from src.models.lung_segmenter import LungSegmenter
 from src.models.map_fuser import MapFuser
 from src.models.mask_processor import MaskProcessor
@@ -23,11 +24,13 @@ class EdemaNet:
         self,
         seg_model_dirs: List[str],
         det_model_dirs: List[str],
-        save_dir: str,
+        lung_extension: Tuple[int, int, int, int] = (50, 50, 50, 150),
+        save_dir: str = 'data/interim_predict',
     ) -> None:
-        self.save_dir = save_dir
         self.seg_model_dirs = seg_model_dirs
         self.det_model_dirs = det_model_dirs
+        self.lung_extension = lung_extension
+        self.save_dir = save_dir
 
     def __call__(
         self,
@@ -55,7 +58,12 @@ class EdemaNet:
             img_dir=img_dir,
         )
 
-        # TODO: Crop
+        # Extract lungs metadata
+        self.compute_lungs_metadata(
+            img_dir=img_dir,
+        )
+
+        # TODO: Crop + add box extension
 
         # TODO: Detection
 
@@ -134,6 +142,38 @@ class EdemaNet:
         mask_path = os.path.join(img_dir, 'mask.png')
         cv2.imwrite(mask_path, mask_clean)
 
+    def compute_lungs_metadata(
+        self,
+        img_dir: str,
+    ) -> dict:
+        mask_path = os.path.join(img_dir, 'mask.png')
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        lung_coords = []
+        for contour in contours:
+            x, y, width, height = cv2.boundingRect(contour)
+            x1, y1, x2, y2 = x, y, x + width, y + height
+            lung_coords.append([x1, y1, x2, y2])
+
+        x1_values, y1_values, x2_values, y2_values = zip(*lung_coords)
+        x1, y1 = min(x1_values), min(y1_values)
+        x2, y2 = max(x2_values), max(y2_values)
+
+        feature_name = 'Lungs'
+        lungs_info = {
+            'Feature ID': FEATURE_MAP[feature_name],
+            'Feature': feature_name,
+            'x1': x1,
+            'y1': y1,
+            'x2': x2,
+            'y2': y2,
+        }
+
+        lungs_info.update(get_box_sizes(x1=x1, y1=y1, x2=x2, y2=y2))
+
+        return lungs_info
+
 
 if __name__ == '__main__':
     data_dir = 'data/interim'
@@ -147,6 +187,7 @@ if __name__ == '__main__':
         det_model_dirs=[
             'models/feature_detection/FasterRCNN',
         ],
+        lung_extension=(50, 50, 50, 150),
         save_dir=results_dir,
     )
 
