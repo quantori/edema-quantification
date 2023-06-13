946dc9e87387c61e210b50833795b6763161eba7
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-13 11:01:59 +0300

    [Feature]: implement preprocessing for object detection

--- src/models/edema_pipeline.py
+++ src/models/edema_pipeline.py
@@ -6,6 +6,7 @@ from glob import glob
 from pathlib import Path
 from typing import List, Tuple
 
+import albumentations as A
 import cv2
 import numpy as np
 import torch
@@ -24,19 +25,23 @@ class EdemaNet:
         self,
         seg_model_dirs: List[str],
         det_model_dirs: List[str],
+        output_size: Tuple[int, int] = (1536, 1536),
         lung_extension: Tuple[int, int, int, int] = (50, 50, 50, 150),
         save_dir: str = 'data/interim_predict',
     ) -> None:
         self.seg_model_dirs = seg_model_dirs
         self.det_model_dirs = det_model_dirs
-        self.lung_extension = lung_extension
+        self.output_size = output_size
+        self.lung_extension = lung_extension  # Order: left (x1), top (y1), right (x2), bottom (y2)
         self.save_dir = save_dir
 
     def __call__(
         self,
         img_path: str,
-    ):
+    ) -> None:
         # Create a directory and copy an image into it
+        img = cv2.imread(img_path)
+        img_height, img_width = img.shape[:2]
         img_name = Path(img_path).stem
         img_dir = os.path.join(self.save_dir, img_name)
         os.makedirs(img_dir, exist_ok=True)
@@ -59,17 +64,35 @@ class EdemaNet:
         )
 
         # Extract lungs metadata
-        self.compute_lungs_metadata(
+        lungs_metadata = self.compute_lungs_metadata(
             img_dir=img_dir,
         )
+        lung_coords_ = (
+            lungs_metadata['x1'],
+            lungs_metadata['y1'],
+            lungs_metadata['x2'],
+            lungs_metadata['y2'],
+        )
+        lungs_coords = self._modify_lung_box(
+            image_height=img_height,
+            image_width=img_width,
+            lung_coords=lung_coords_,
+            lung_extension=self.lung_extension,
+        )
 
-        # TODO: Crop + add box extension
+        # Process image that is used by an object detector
+        self.process_image(
+            img=img,
+            x1=lungs_coords[0],
+            y1=lungs_coords[1],
+            x2=lungs_coords[2],
+            y2=lungs_coords[3],
+            output_size=self.output_size,
+        )
 
         # TODO: Detection
 
         # TODO: Classification
-
-        return 1
 
     def segment_lungs(
         self,
@@ -161,7 +184,7 @@ class EdemaNet:
         x2, y2 = max(x2_values), max(y2_values)
 
         feature_name = 'Lungs'
-        lungs_info = {
+        lungs_metadata = {
             'Feature ID': FEATURE_MAP[feature_name],
             'Feature': feature_name,
             'x1': x1,
@@ -170,9 +193,88 @@ class EdemaNet:
             'y2': y2,
         }
 
-        lungs_info.update(get_box_sizes(x1=x1, y1=y1, x2=x2, y2=y2))
+        lungs_metadata.update(get_box_sizes(x1=x1, y1=y1, x2=x2, y2=y2))
 
-        return lungs_info
+        return lungs_metadata
+
+    def process_image(
+        self,
+        img: np.ndarray,
+        x1: int,
+        y1: int,
+        x2: int,
+        y2: int,
+        output_size: Tuple[int, int],
+    ) -> np.ndarray:
+        transform = A.Compose(
+            [
+                A.Crop(
+                    x_min=x1,
+                    y_min=y1,
+                    x_max=x2,
+                    y_max=y2,
+                    always_apply=True,
+                ),
+                A.LongestMaxSize(
+                    max_size=max(output_size),
+                    interpolation=4,
+                    always_apply=True,
+                ),
+                A.PadIfNeeded(
+                    min_width=output_size[0],
+                    min_height=output_size[1],
+                    position='center',
+                    border_mode=cv2.BORDER_CONSTANT,
+                    value=0,
+                    always_apply=True,
+                ),
+            ],
+        )
+
+        transformed = transform(image=img)
+        img_out = transformed['image']
+
+        return img_out
+
+    @staticmethod
+    def _modify_lung_box(
+        image_height: int,
+        image_width: int,
+        lung_coords: Tuple[int, int, int, int],
+        lung_extension: Tuple[int, int, int, int],
+    ) -> Tuple[int, int, int, int]:
+        x1 = lung_coords[0] - lung_extension[0]
+        y1 = lung_coords[1] - lung_extension[1]
+        x2 = lung_coords[2] + lung_extension[2]
+        y2 = lung_coords[3] + lung_extension[3]
+
+        # Check if the box coordinates exceed image dimensions
+        if x1 < 0:
+            logging.warning(f'x1 = {x1} exceeds the left edge of the image = {0}')
+        if y1 < 0:
+            logging.warning(f'y1 = {y1} exceeds the top edge of the image = {0}')
+        if x2 > image_width:
+            logging.warning(f'x2 = {x2} exceeds the right edge of the image = {image_width}')
+        if y2 > image_height:
+            logging.warning(f'y2 = {y2} exceeds the bottom edge of the image = {image_height}')
+
+        # Check if x2 is greater than x1 and y2 is greater than y1
+        if x2 <= x1:
+            logging.warning(
+                f'x2 = {x2} is not greater than x1 = {x1}',
+            )
+        if y2 <= y1:
+            logging.warning(
+                f'y2 = {y2} is not greater than y1 = {y1}',
+            )
+
+        # Clip coordinates to image dimensions if necessary
+        x1 = max(0, x1)
+        y1 = max(0, y1)
+        x2 = min(image_width, x2)
+        y2 = min(image_height, y2)
+
+        return x1, y1, x2, y2
 
 
 if __name__ == '__main__':
@@ -187,6 +289,7 @@ if __name__ == '__main__':
         det_model_dirs=[
             'models/feature_detection/FasterRCNN',
         ],
+        output_size=(1536, 1536),
         lung_extension=(50, 50, 50, 150),
         save_dir=results_dir,
     )
@@ -204,6 +307,6 @@ if __name__ == '__main__':
 
     for img_path in img_paths:
         print(f'Image: {Path(img_path).stem}')
-        result = edema_net(img_path)
+        edema_net(img_path)
 
     print('Complete')
