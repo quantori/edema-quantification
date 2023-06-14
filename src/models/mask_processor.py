1fea647cc8e4cdf3df0ace60c20492e76cea52b0
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-14 21:23:18 +0300

    [Fix]: add __init__ method

--- src/models/mask_processor.py
+++ src/models/mask_processor.py
@@ -10,37 +10,40 @@ class MaskProcessor:
     It provides methods for binarization, smoothing, and artifact removal of masks.
     """
 
-    @staticmethod
-    def binarize_image(
-        image: np.ndarray,
+    def __init__(
+        self,
         threshold_method: str = 'otsu',
-    ) -> np.ndarray:
-        assert threshold_method in [
-            'otsu',
-            'triangle',
-        ], f'Invalid threshold_method: {threshold_method}'
+        kernel_size: Tuple[int, int] = (7, 7),
+    ) -> None:
+        self.threshold_method = threshold_method
+        self.kernel_size = kernel_size
 
-        if threshold_method == 'otsu':
+    def binarize_image(
+        self,
+        image: np.ndarray,
+    ) -> np.ndarray:
+        if self.threshold_method == 'otsu':
             _, mask = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        elif threshold_method == 'triangle':
+        elif self.threshold_method == 'triangle':
             _, mask = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_TRIANGLE)
+        else:
+            raise ValueError(f'Invalid threshold_method: {self.threshold_method}')
 
         return mask
 
-    @staticmethod
     def smooth_mask(
+        self,
         mask: np.ndarray,
-        kernel_size: Tuple[int, int] = (7, 7),
     ) -> np.ndarray:
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, kernel_size)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, self.kernel_size)
         opened_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
         closed_mask = cv2.morphologyEx(opened_mask, cv2.MORPH_CLOSE, kernel)
         filled_mask = cv2.morphologyEx(closed_mask, cv2.MORPH_DILATE, kernel)
 
         return filled_mask
 
-    @staticmethod
     def remove_artifacts(
+        self,
         mask: np.ndarray,
     ) -> np.ndarray:
         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -50,7 +53,7 @@ class MaskProcessor:
 
         biggest_contours = [cnt for cnt, area in zip(contours, areas) if area in sorted_areas]
 
-        new_mask = np.zeros_like(mask)
-        new_mask = cv2.drawContours(new_mask, biggest_contours, -1, 255, thickness=cv2.FILLED)
+        mask_new = np.zeros_like(mask)
+        mask_new = cv2.drawContours(mask_new, biggest_contours, -1, 255, thickness=cv2.FILLED)
 
-        return new_mask
+        return mask_new
