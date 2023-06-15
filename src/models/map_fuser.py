471cb08a64134f0e2edc20cf0053376d29803dd2
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-15 21:29:29 +0300

    [Fix]: speed up performance of conditional_probability_fusion()

--- src/models/map_fuser.py
+++ src/models/map_fuser.py
@@ -3,10 +3,10 @@ import numpy as np
 
 
 class MapFuser:
-    """MaskFuser is a class for fusing multiple probability maps into a single fused mask.
+    """MapFuser is a class for fusing multiple probability maps into a single fused map.
 
     It supports adding probability maps, calculating the conditional probability fusion,
-    and obtaining the fused mask.
+    and obtaining the fused map.
     """
 
     def __init__(self):
@@ -28,10 +28,7 @@ class MapFuser:
 
         self.prob_maps.append(prob_map)
 
-    def conditional_probability_fusion(
-        self,
-        scale_output: bool = True,
-    ):
+    def conditional_probability_fusion(self, scale_output=True):
         # Ensure at least one map has been added
         assert len(self.prob_maps) > 0, 'No prob_maps have been added'
 
@@ -39,29 +36,32 @@ class MapFuser:
             return self.prob_maps[0]
 
         # Calculate the conditional probability fusion
-        fused_map = np.zeros_like(self.prob_maps[0])
-
-        for y in range(fused_map.shape[0]):
-            for x in range(fused_map.shape[1]):
-                # Calculate the conditional probability of foreground
-                prob_foreground = np.prod([mask[y, x] for mask in self.prob_maps])
-
-                # Calculate the conditional probability of background
-                prob_background = 1 - prob_foreground
-
-                # Normalize the probabilities
-                total_prob = prob_foreground + prob_background
-                if total_prob > 0:
-                    prob_foreground /= total_prob
-                    prob_background /= total_prob
-
-                # Assign the fused probability to the foreground class
-                fused_map[y, x] = prob_foreground
+        img_height, img_width = self.prob_maps[0].shape[:2]
+        prob_product = np.prod(self.prob_maps, axis=0)
+        fused_map = np.array(
+            [
+                [self.calculate_probability(prob_product[y, x]) for x in range(img_width)]
+                for y in range(img_height)
+            ],
+        )
 
         if scale_output:
             fused_map = (fused_map * 255.0).astype(np.uint8)
 
         return fused_map
+
+    @staticmethod
+    def calculate_probability(prob_foreground: float) -> float:
+        # Calculate the conditional probability of background
+        prob_background = 1 - prob_foreground
+
+        # Normalize the probabilities
+        total_prob = prob_foreground + prob_background
+        if total_prob > 0:
+            prob_foreground /= total_prob
+            prob_background /= total_prob
+
+        return prob_foreground
 
 
 if __name__ == '__main__':
@@ -70,14 +70,15 @@ if __name__ == '__main__':
 
     # Add prob_map paths
     map_paths = [
-        'data/interim_lungs/MAnet/10013643_58785837.png',
         'data/interim_lungs/DeepLabV3/10013643_58785837.png',
-        'data/interim_lungs/Unet++/10013643_58785837.png',
+        'data/interim_lungs/FPN/10013643_58785837.png',
+        'data/interim_lungs/MAnet/10013643_58785837.png',
     ]
     for map_path in map_paths:
         prob_map = cv2.imread(map_path, cv2.IMREAD_GRAYSCALE)
         fuser.add_prob_map(prob_map)
     fused_map = fuser.conditional_probability_fusion(scale_output=True)
+    print(np.mean(fused_map), np.std(fused_map))
 
     # Process probability map
     from src.models.mask_processor import MaskProcessor
@@ -86,4 +87,5 @@ if __name__ == '__main__':
     mask_bin = processor.binarize_image(image=fused_map)
     mask_smooth = processor.smooth_mask(mask=mask_bin)
     mask_clean = processor.remove_artifacts(mask=mask_smooth)
-    print('text')
+
+    print('Complete')
