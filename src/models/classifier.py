63b9e10f085c5f6fc52eb6e4affd6fac85f506af
Anton Makoveev <makoveev90@gmail.com>
2023-06-16 10:33:17 +0200

    [Fix]: remove the abstract class and inheritance.

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -1,25 +1,12 @@
-# type: ignore
-
-from abc import ABC, abstractclassmethod
-from typing import Generic, List, Tuple, TypeVar
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
 
 from src.data.utils_sly import CLASS_MAP
 
-T = TypeVar('T')
 
-
-class EdemaClassifier(ABC, Generic[T]):
-    """Abstract base class for edema severity classifiers."""
-
-    @abstractclassmethod
-    def classify(self, *args, **kwargs) -> T:
-        """Called when edma severity needs to be classified."""
-
-
-class HCEdemaClassifier(EdemaClassifier[pd.DataFrame]):
+class EdemaClassifier:
     """Hard-coded edema classifier."""
 
     def __init__(self) -> None:
@@ -37,7 +24,7 @@ class HCEdemaClassifier(EdemaClassifier[pd.DataFrame]):
         image_names = self._get_unique_image_names(df_in)
         for idx, image_name in enumerate(image_names):
             df_temp = df_in[df_in['Image name'] == image_name]
-            edema_severity, feature, confidence = HCEdemaClassifier._get_edema_severity(df_temp)
+            edema_severity, feature, confidence = EdemaClassifier._get_edema_severity(df_temp)
             new_row = {
                 'ID': idx,
                 'Image path': df_temp['Image path'].iloc[0],
@@ -59,38 +46,53 @@ class HCEdemaClassifier(EdemaClassifier[pd.DataFrame]):
     def _get_edema_severity(df: pd.DataFrame) -> Tuple[str, float, float]:
         features = df['Feature'].unique()
         if 'Bat' in features or 'Infiltrate' in features:
-            df_alv_edema = df[(df['Feature'] == 'Bat') | (df['Feature'] == 'Infiltrate')]
-            max_row = df_alv_edema.nlargest(1, ['Confidence'])
-            return 'Alveolar edema', max_row['Feature'].values[0], max_row['Confidence'].values[0]
+            edema_severity, feature, confidence = EdemaClassifier._get_alveolar_edema(df)
+            return edema_severity, feature, confidence
 
         elif 'Effusion' in features or 'Kerley' in features or 'Cuffing' in features:
-            df_int_edema = df[
-                (df['Feature'] == 'Effusion')
-                | (df['Feature'] == 'Kerley')
-                | (df['Feature'] == 'Cuffing')
-            ]
-            max_row = df_int_edema.nlargest(1, ['Confidence'])
-            return (
-                'Interstitial edema',
-                max_row['Feature'].values[0],
-                max_row['Confidence'].values[0],
-            )
+            edema_severity, feature, confidence = EdemaClassifier._get_interstitial_edema(df)
+            return edema_severity, feature, confidence
 
         elif 'Cephalization' in features:
-            df_cong_edema = df[df['Feature'] == 'Cephalization']
-            max_row = df_cong_edema.nlargest(1, ['Confidence'])
-            return (
-                'Vascular congestion',
-                max_row['Feature'].values[0],
-                max_row['Confidence'].values[0],
-            )
+            edema_severity, feature, confidence = EdemaClassifier._get_vascular_congestion(df)
+            return edema_severity, feature, confidence
 
         else:
             return 'No edema', np.nan, np.nan
 
+    @staticmethod
+    def _get_alveolar_edema(df: pd.DataFrame) -> Tuple[str, float, float]:
+        df_alv_edema = df[(df['Feature'] == 'Bat') | (df['Feature'] == 'Infiltrate')]
+        max_row = df_alv_edema.nlargest(1, ['Confidence'])
+        return 'Alveolar edema', max_row['Feature'].values[0], max_row['Confidence'].values[0]
+
+    @staticmethod
+    def _get_interstitial_edema(df: pd.DataFrame) -> Tuple[str, float, float]:
+        df_int_edema = df[
+            (df['Feature'] == 'Effusion')
+            | (df['Feature'] == 'Kerley')
+            | (df['Feature'] == 'Cuffing')
+        ]
+        max_row = df_int_edema.nlargest(1, ['Confidence'])
+        return (
+            'Interstitial edema',
+            max_row['Feature'].values[0],
+            max_row['Confidence'].values[0],
+        )
+
+    @staticmethod
+    def _get_vascular_congestion(df: pd.DataFrame) -> Tuple[str, float, float]:
+        df_cong_edema = df[df['Feature'] == 'Cephalization']
+        max_row = df_cong_edema.nlargest(1, ['Confidence'])
+        return (
+            'Vascular congestion',
+            max_row['Feature'].values[0],
+            max_row['Confidence'].values[0],
+        )
+
 
 if __name__ == '__main__':
     df = pd.read_excel('./data/coco/test/predictions.xlsx')
-    classifier = HCEdemaClassifier()
+    classifier = EdemaClassifier()
     df_o = classifier.classify(df)
     print(df_o)
