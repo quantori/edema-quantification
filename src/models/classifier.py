a7e6d23b8cfc10eaef4a8cc67a50cc2b51313677
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-16 13:58:18 +0300

    [Fix]: refactor classification pipeline

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -12,7 +12,10 @@ class EdemaClassifier:
     def __init__(self) -> None:
         self._output: List = []
 
-    def classify(self, df_in: pd.DataFrame) -> pd.DataFrame:
+    def classify(
+        self,
+        df_in: pd.DataFrame,
+    ) -> pd.DataFrame:
         """The main classification function.
 
         Args:
@@ -21,30 +24,32 @@ class EdemaClassifier:
         Returns:
             DataFrame with identified edema severity class.
         """
-        image_names = self._get_unique_image_names(df_in)
-        for idx, image_name in enumerate(image_names):
-            df_temp = df_in[df_in['Image name'] == image_name]
-            edema_severity, feature, confidence = EdemaClassifier._get_edema_severity(df_temp)
+        # TODO: ensure that EdemaClassifier can process empty dataframes
+        img_groups = df_in.groupby('Image name')
+        for image_name, df_img in img_groups:
+            # FIXME: Extraction of feature and its confidence is not necessary
+            # FIXME: I think it is enough if you add two columns (Class ID and Class)
+            edema_severity, feature, confidence = EdemaClassifier._get_edema_severity(df_img)
             new_row = {
-                'ID': idx,
-                'Image path': df_temp['Image path'].iloc[0],
+                'Image path': df_img['Image path'].iloc[0],
                 'Image name': image_name,
-                'Image height': df_temp['Image height'].iloc[0],
-                'Image width': df_temp['Image width'].iloc[0],
+                'Image height': df_img['Image height'].iloc[0],
+                'Image width': df_img['Image width'].iloc[0],
                 'Feature': feature,
                 'Confidence': confidence,
                 'Class ID': CLASS_MAP[edema_severity],
                 'Class': edema_severity,
             }
             self._output.append(new_row)
-        return pd.DataFrame(self._output)
 
-    def _get_unique_image_names(self, df: pd.DataFrame) -> np.ndarray:
-        return pd.unique(df['Image name'])
+        # FIXME: df_in and df_out should have the same length
+        df_out = pd.DataFrame(self._output)
+
+        return df_out
 
     @staticmethod
     def _get_edema_severity(df: pd.DataFrame) -> Tuple[str, float, float]:
-        features = df['Feature'].unique()
+        features = list(df['Feature'].unique())
         if 'Bat' in features or 'Infiltrate' in features:
             edema_severity, feature, confidence = EdemaClassifier._get_alveolar_edema(df)
             return edema_severity, feature, confidence
@@ -61,34 +66,26 @@ class EdemaClassifier:
             return 'No edema', np.nan, np.nan
 
     @staticmethod
-    def _get_alveolar_edema(df: pd.DataFrame) -> Tuple[str, float, float]:
-        df_alv_edema = df[(df['Feature'] == 'Bat') | (df['Feature'] == 'Infiltrate')]
-        max_row = df_alv_edema.nlargest(1, ['Confidence'])
-        return 'Alveolar edema', max_row['Feature'].values[0], max_row['Confidence'].values[0]
+    def _get_vascular_congestion(df: pd.DataFrame) -> Tuple[str, float, float]:
+        df_edema = df[df['Feature'] == 'Cephalization']
+        max_row = df_edema.nlargest(1, ['Confidence'])
+        return 'Vascular congestion', max_row['Feature'].values[0], max_row['Confidence'].values[0]
 
     @staticmethod
     def _get_interstitial_edema(df: pd.DataFrame) -> Tuple[str, float, float]:
-        df_int_edema = df[
+        df_edema = df[
             (df['Feature'] == 'Effusion')
             | (df['Feature'] == 'Kerley')
             | (df['Feature'] == 'Cuffing')
         ]
-        max_row = df_int_edema.nlargest(1, ['Confidence'])
-        return (
-            'Interstitial edema',
-            max_row['Feature'].values[0],
-            max_row['Confidence'].values[0],
-        )
+        max_row = df_edema.nlargest(1, ['Confidence'])
+        return 'Interstitial edema', max_row['Feature'].values[0], max_row['Confidence'].values[0]
 
     @staticmethod
-    def _get_vascular_congestion(df: pd.DataFrame) -> Tuple[str, float, float]:
-        df_cong_edema = df[df['Feature'] == 'Cephalization']
-        max_row = df_cong_edema.nlargest(1, ['Confidence'])
-        return (
-            'Vascular congestion',
-            max_row['Feature'].values[0],
-            max_row['Confidence'].values[0],
-        )
+    def _get_alveolar_edema(df: pd.DataFrame) -> Tuple[str, float, float]:
+        df_edema = df[(df['Feature'] == 'Bat') | (df['Feature'] == 'Infiltrate')]
+        max_row = df_edema.nlargest(1, ['Confidence'])
+        return 'Alveolar edema', max_row['Feature'].values[0], max_row['Confidence'].values[0]
 
 
 if __name__ == '__main__':
