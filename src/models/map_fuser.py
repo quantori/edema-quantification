b02dc13c991366c74079317d94a4cad0fcff0304
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-06-28 18:26:17 +0300

    [Fix]: minor removal

--- src/models/map_fuser.py
+++ src/models/map_fuser.py
@@ -80,7 +80,6 @@ if __name__ == '__main__':
         prob_map = cv2.imread(map_path, cv2.IMREAD_GRAYSCALE)
         fuser.add_prob_map(prob_map)
     fused_map = fuser.conditional_probability_fusion(scale_output=True)
-    print(np.mean(fused_map), np.std(fused_map))
 
     # Process probability map
     from src.models.mask_processor import MaskProcessor
