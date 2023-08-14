48bd06eec9b9c7dbad8b79d45d6a46829d849cf8
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2023-08-14 12:14:21 +0300

    [Feature]: change module imports for object_detection_metrics

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .object_detection_metrics.lib.BoundingBox import BoundingBox  # noqa: F401
+from .object_detection_metrics.lib.BoundingBoxes import BoundingBoxes  # noqa: F401
+from .object_detection_metrics.lib.Evaluator import Evaluator  # noqa: F401
+from .object_detection_metrics.lib.utils import BBFormat, BBType, CoordinatesType  # noqa: F401
